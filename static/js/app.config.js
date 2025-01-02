44624c18d705f9f094c45dddde930bc3b0012b85
Sebastian Pauka <spauka@gmail.com>
2025-01-02 22:08:46 +1000

    Remove unused config options

--- static/js/app.config.js
+++ static/js/app.config.js
@@ -33,11 +33,6 @@
  */
 	$.navAsAjax = true;
 /*
- * GLOBAL: Sound Config
- */
-	$.sound_path = "sound/";
-	$.sound_on = false;
-/*
  * Impacts the responce rate of some of the responsive elements (lower
  * value affects CPU but improves speed)
  */
@@ -58,106 +53,10 @@
  */
 	enableMobileWidgets = false,
 /*
- * Turn on fast click for mobile devices?
- * Enable this to activate fastclick plugin
- * dependency: js/plugin/fastclick/fastclick.js
- */
-	fastClick = false,
-/*
  * These elements are ignored during DOM object deletion for ajax version
  * It will delete all objects during page load with these exceptions:
  */
-	ignore_key_elms = ["#header, #left-panel, #main, div.page-footer, #shortcut, #divSmallBoxes, #divMiniIcons, #divbigBoxes, #voiceModal, script"],
-/*
- * VOICE COMMAND CONFIG
- * dependency: voicecommand.js
- */
-	voice_command = false,
-/*
- * Turns on speech without asking
- */
-	voice_command_auto = false,
-/*
- * 	Sets the language to the default 'en-US'. (supports over 50 languages
- * 	by google)
- *
- *  Afrikaans         ['af-ZA']
- *  Bahasa Indonesia  ['id-ID']
- *  Bahasa Melayu     ['ms-MY']
- *  Català            ['ca-ES']
- *  Čeština           ['cs-CZ']
- *  Deutsch           ['de-DE']
- *  English           ['en-AU', 'Australia']
- *                    ['en-CA', 'Canada']
- *                    ['en-IN', 'India']
- *                    ['en-NZ', 'New Zealand']
- *                    ['en-ZA', 'South Africa']
- *                    ['en-GB', 'United Kingdom']
- *                    ['en-US', 'United States']
- *  Español           ['es-AR', 'Argentina']
- *                    ['es-BO', 'Bolivia']
- *                    ['es-CL', 'Chile']
- *                    ['es-CO', 'Colombia']
- *                    ['es-CR', 'Costa Rica']
- *                    ['es-EC', 'Ecuador']
- *                    ['es-SV', 'El Salvador']
- *                    ['es-ES', 'España']
- *                    ['es-US', 'Estados Unidos']
- *                    ['es-GT', 'Guatemala']
- *                    ['es-HN', 'Honduras']
- *                    ['es-MX', 'México']
- *                    ['es-NI', 'Nicaragua']
- *                    ['es-PA', 'Panamá']
- *                    ['es-PY', 'Paraguay']
- *                    ['es-PE', 'Perú']
- *                    ['es-PR', 'Puerto Rico']
- *                    ['es-DO', 'República Dominicana']
- *                    ['es-UY', 'Uruguay']
- *                    ['es-VE', 'Venezuela']
- *  Euskara           ['eu-ES']
- *  Français          ['fr-FR']
- *  Galego            ['gl-ES']
- *  Hrvatski          ['hr_HR']
- *  IsiZulu           ['zu-ZA']
- *  Íslenska          ['is-IS']
- *  Italiano          ['it-IT', 'Italia']
- *                    ['it-CH', 'Svizzera']
- *  Magyar            ['hu-HU']
- *  Nederlands        ['nl-NL']
- *  Norsk bokmål      ['nb-NO']
- *  Polski            ['pl-PL']
- *  Português         ['pt-BR', 'Brasil']
- *                    ['pt-PT', 'Portugal']
- *  Română            ['ro-RO']
- *  Slovenčina        ['sk-SK']
- *  Suomi             ['fi-FI']
- *  Svenska           ['sv-SE']
- *  Türkçe            ['tr-TR']
- *  български         ['bg-BG']
- *  Pусский           ['ru-RU']
- *  Српски            ['sr-RS']
- *  한국어          ['ko-KR']
- *  中文                            ['cmn-Hans-CN', '普通话 (中国大陆)']
- *                    ['cmn-Hans-HK', '普通话 (香港)']
- *                    ['cmn-Hant-TW', '中文 (台灣)']
- *                    ['yue-Hant-HK', '粵語 (香港)']
- *  日本語                         ['ja-JP']
- *  Lingua latīna     ['la']
- */
-	voice_command_lang = 'en-US',
-/*
- * 	Use localstorage to remember on/off (best used with HTML Version)
- */
-	voice_localStorage = false;
-/*
- * Voice Commands
- * Defines all voice command variables and functions
- */
- 	if (voice_command) {
-		var commands = {
-			'show dashboard' : function() { window.location.hash = "dashboard" },
-		};
-	};
+	ignore_key_elms = ["#header, #left-panel, #main, div.page-footer, #shortcut, #divSmallBoxes, #divMiniIcons, #divbigBoxes, #voiceModal, script"];
 
 /*
  * Define data uri
