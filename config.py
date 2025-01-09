8aa4ec128f24fb556f2034a1b4cfb9b2748e9198
Sebastian Pauka <spauka@gmail.com>
2025-01-09 17:40:20 +1000

    Option to load config from ENV

--- config.py
+++ config.py
@@ -1,15 +1,19 @@
+import os
 import json
 from pathlib import Path
 from dataclasses import dataclass, field
 from dataclass_wizard import JSONFileWizard
 
-CONFIG_FILE = Path(__file__).parent / "config.json"
+if "THERM_CONFIG" in os.environ:
+    CONFIG_FILE = os.environ["THERM_CONFIG"]
+else:
+    CONFIG_FILE = Path(__file__).parent / "config.json"
 
 @dataclass
 class Config(JSONFileWizard):
     db: str
     uri_base: str
-    
+
     debug: bool
     track_modifications: bool
     record_queries: bool
@@ -18,3 +22,4 @@ config = Config.from_json_file(CONFIG_FILE)
 # Pass config accesses to the config file
 def __getattr__(name):
     return getattr(config, name)
+
