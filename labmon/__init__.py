5cd91faac4175061d752896fdc4c2e8282d4d0e4
Sebastian Pauka <spauka@gmail.com>
2025-01-21 00:46:08 +1100

    Add uwsgi rewriter

--- labmon/__init__.py
+++ labmon/__init__.py
@@ -3,19 +3,18 @@ import json
 from pathlib import Path
 
 from flask import Flask, render_template
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 
 import labmon.db as db
 
 DEFAULT_CONF_LOC = Path("~").expanduser()
 CONF_LOC = os.environ.get("THERM_CONFIG", DEFAULT_CONF_LOC)
 
+
 def create_app():
     # Create and configure the app
-    app = Flask(
-        __name__,
-        instance_relative_config=True,
-        instance_path=CONF_LOC
-    )
+    app = Flask(__name__, instance_relative_config=True, instance_path=CONF_LOC)
 
     # Load config file
     app.config.from_file("labmon_config.json", load=json.load)
@@ -27,9 +26,11 @@ def create_app():
     @app.route("/")
     def blank():
         return render_template("blank.html", title=app.name)
-    
+
     # Load fridge data
     from . import fridge_data
+
     app.register_blueprint(fridge_data.fridge_bp)
 
+    app.wsgi_app = ProxyFix(app.wsgi_app)
     return app
