e8046a1f3ea93ab825efa6b148752363ec4c60fc
Sebastian Pauka <spauka@gmail.com>
2025-01-25 14:07:08 +1100

    Fix formatting

--- labmon/db/db.py
+++ labmon/db/db.py
@@ -1,7 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
 
+
 class Base(DeclarativeBase, MappedAsDataclass):
     pass
+
 
 db = SQLAlchemy(model_class=Base)
