d0015f238dd03ae40a1dd8c00dce597215fa7c52
Sebastian Pauka <spauka@gmail.com>
2025-02-06 13:00:10 +1000

    Add exit and edit message to config on first run

--- labmon/config.py
+++ labmon/config.py
@@ -1,24 +1,88 @@
+# pylint: disable=invalid-name
+import logging
 import os
-import json
-from pathlib import Path
+import sys
 from dataclasses import dataclass, field
-from dataclass_wizard import JSONFileWizard
+from pathlib import Path
 
-if "THERM_CONFIG" in os.environ:
-    CONFIG_FILE = os.environ["THERM_CONFIG"]
+from dataclass_wizard import JSONFileWizard, JSONWizard
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_CONF_LOC = Path("~").expanduser()
+CONF_LOC = Path(os.environ.get("THERM_CONFIG", DEFAULT_CONF_LOC))
+CONFIG_FILE = CONF_LOC / "labmon_config.json"
+
+
+@dataclass(frozen=True)
+class ServerConfig:
+    ENABLED: bool = False
+    SQLALCHEMY_DATABASE_URI: str = "postgresql+psycopg://therm@/therm"
+    APPLICATION_ROOT: str = "/"
+    DEBUG: bool = False
+    SQLALCHEMY_RECORD_QUERIES: bool = False
+    SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
+
+
+@dataclass(frozen=True)
+class BlueForsUploadConfig:
+    LOG_DIR: str = "C:\\BlueFors_Logs"
+    MAX_AGE: float = 180.0  # Maximum age of data in seconds
+    SENSORS: dict[str, int] = field(
+        default_factory=lambda: {
+            "Fifty_K": 1,
+            "Four_K": 2,
+            "Magnet": 3,
+            "Still": 5,
+            "MXC": 6,
+        }
+    )
+
+
+@dataclass(frozen=True)
+class UploadConfig:
+    ENABLED: bool = True
+    BASE_URL: str = "https://qsyd.sydney.edu.au/data"
+    FRIDGE: str = "?"
+    BLUEFORS_CONFIG: BlueForsUploadConfig = BlueForsUploadConfig()
+
+
+@dataclass(frozen=True)
+class Config(JSONFileWizard, JSONWizard):
+    class _(JSONWizard.Meta):
+        key_transform_with_dump = "NONE"
+
+    SERVER: ServerConfig = ServerConfig()
+    UPLOAD: UploadConfig = UploadConfig()
+
+
+if not CONFIG_FILE.exists():
+    logger.warning(
+        "Config file doesn't exist. Creating new file at %s.", str(CONFIG_FILE)
+    )
+    config: Config = Config()
+    config.to_json_file(CONFIG_FILE, indent=2)
+
+    # Print message and exit
+    message = (
+        f"Created config file at {CONFIG_FILE}.\n"
+        f"Please fill in details and start again."
+    )
+    print(message)
+    logger.error(message)
+    sys.exit(0)
 else:
-    CONFIG_FILE = Path(__file__).parent / "config.json"
+    config: Config = Config.from_json_file(CONFIG_FILE)
+logger.info("Loaded config file %s", str(CONFIG_FILE))
+logger.debug("Config parameters: %r", config)
 
-@dataclass
-class Config(JSONFileWizard):
-    db: str
-
-    debug: bool
-    track_modifications: bool
-    record_queries: bool
-config = Config.from_json_file(CONFIG_FILE)
 
 # Pass config accesses to the config file
 def __getattr__(name):
     return getattr(config, name)
 
+
+if __name__ == "__main__":
+    import pprint
+
+    pprint.pprint(config)
