8afe6378d03171d20fa646b2d88da1092d6ec0b3
Sebastian Pauka <spauka@gmail.com>
2025-02-06 14:18:31 +1000

    Debugging script for BF uploader

--- labmon/uploaders/bluefors.py
+++ labmon/uploaders/bluefors.py
@@ -1,6 +1,25 @@
+from datetime import datetime, timedelta, timezone
+from pprint import pprint
+
 from ..utility.logging import logging, set_logging
 from .bluefors_tc import BlueForsTempMonitor
 
 if __name__ == "__main__":
     set_logging(logging.DEBUG)
     fs = BlueForsTempMonitor()
+
+    # For debugging purposes, set the latest time to some value in our dataset
+    fs.latest = datetime(
+        2025,
+        2,
+        5,
+        22,
+        0,
+        0,
+        tzinfo=timezone(timedelta(seconds=39600)),
+    )
+
+    # Go through all the datasets
+    poll = True
+    while poll:
+        poll = fs.poll()
