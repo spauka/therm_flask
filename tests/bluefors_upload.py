5890d83f703fd76e7365571401ca285d79d6838e
Sebastian Pauka <spauka@gmail.com>
2025-02-06 17:56:16 +1000

    Make test use the BlueFors collective uploader

--- tests/bluefors_upload.py
+++ tests/bluefors_upload.py
@@ -1,31 +1,25 @@
 from datetime import datetime, timedelta, timezone
 from pprint import pprint
 
-from labmon.uploaders.bluefors_cp import BlueForsCompressorMonitor
-from labmon.uploaders.bluefors_mg import BlueForsMaxiGaugeMonitor
-from labmon.uploaders.bluefors_tc import BlueForsTempMonitor
-from labmon.utility.logging import logging, set_logging
+from labmon.uploaders.bluefors_upload import BlueForsMonitor
 
 if __name__ == "__main__":
-    set_logging(logging.DEBUG)
-    fs = BlueForsTempMonitor()
-    cp = BlueForsCompressorMonitor(supp="Compressor")
-    mg = BlueForsMaxiGaugeMonitor()
+    fs = BlueForsMonitor()
 
     # For debugging purposes, set the latest time to some value in our dataset
     fs.latest = datetime(
         2025,
         2,
         5,
-        22,
+        23,
         0,
         0,
         tzinfo=timezone(timedelta(seconds=39600)),
     )
-    cp.latest = fs.latest
-    mg.latest = fs.latest
+    for monitor in fs.monitor:
+        monitor.latest = fs.latest
 
     # Go through all the datasets
     poll = True
     while poll:
-        poll = mg.poll()
+        poll = fs.poll()
