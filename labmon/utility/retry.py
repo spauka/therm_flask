57e48805e2417166a3db214090196ca08986608b
Sebastian Pauka <spauka@gmail.com>
2025-03-18 15:47:09 +1100

    Make retry decorator work with coroutines

--- labmon/utility/retry.py
+++ labmon/utility/retry.py
@@ -1,31 +1,80 @@
 import logging
 import random
+from inspect import iscoroutinefunction
+from functools import wraps
+from asyncio import sleep as asleep
 from time import sleep
 
 logger = logging.getLogger(__name__)
 
 
 def retry(
-    starting_retry_wait=1.0,
-    multiplier=1.5,
-    max_wait=300.0,
+    starting_retry_wait: float = 1.0,
+    multiplier: float = 1.5,
+    max_wait: float = 300.0,
+    max_retry: int = 0,
     exception: tuple[type[Exception], ...] | type[Exception] = Exception,
 ):
     """
     Decorator to add timeout with exponential backoff to function
-    on exception
+    on exception. If the wrapped function is asynchronous, use an async sleep
     """
 
     def retry_func(f):
+        if iscoroutinefunction(f):
+            # Wrap an asynchronous coroutine
+            @wraps(f)
+            async def async_f_with_retry(
+                *args, retry_wait=starting_retry_wait, retry_count=0, max_wait=max_wait, **kwargs
+            ):
+                while True:
+                    try:
+                        return await f(*args, **kwargs)
+                    except Exception as e:  # pylint: disable=broad-exception-caught
+                        # If we've exceeded the max retries, or if the exception is not a known
+                        # type, pass the exception forward.
+                        if max_retry > 0 and retry_count == max_retry:
+                            raise e
+                        elif not isinstance(e, exception):
+                            raise e
+
+                        # Otherwise, wait for wait-time and retry the function
+                        wait_time = (retry_wait / 2) + random.uniform(1, retry_wait)
+                        retry_wait = min(retry_wait * multiplier, max_wait)
+                        logger.error(
+                            (
+                                "Retrying function %s due to exception %s. "
+                                "Attempt %d. Waiting %.2f to try again.",
+                            ),
+                            f.__name__,
+                            e.__class__.__name__,
+                            retry_count,
+                            wait_time,
+                            exc_info=e,
+                        )
+                        await asleep(wait_time)
+
+            return async_f_with_retry
+
+        # Otherwise the function is synchronous, return the synchronous version of the retry function
+        @wraps(f)
         def f_with_retry(
             *args, retry_wait=starting_retry_wait, retry_count=0, max_wait=max_wait, **kwargs
         ):
-            try:
-                return f(*args, **kwargs)
-            except Exception as e:  # pylint: disable=broad-exception-caught
-                if isinstance(e, exception):
+            while True:
+                try:
+                    return f(*args, **kwargs)
+                except Exception as e:  # pylint: disable=broad-exception-caught
+                    # If we've exceeded the max retries, or if the exception is not a known
+                    # type, pass the exception forward.
+                    if max_retry > 0 and retry_count == max_retry:
+                        raise e
+                    elif not isinstance(e, exception):
+                        raise e
+
+                    # Otherwise, wait for wait-time and retry the function
                     wait_time = (retry_wait / 2) + random.uniform(1, retry_wait)
-                    next_retry_wait = min(retry_wait * multiplier, max_wait)
+                    retry_wait = min(retry_wait * multiplier, max_wait)
                     logger.error(
                         (
                             "Retrying function %s due to exception %s. "
@@ -38,14 +87,6 @@ def retry(
                         exc_info=e,
                     )
                     sleep(wait_time)
-                else:
-                    raise e
-            return f_with_retry(
-                *args,
-                retry_wait=next_retry_wait,
-                retry_count=retry_count + 1,
-                **kwargs,
-            )
 
         return f_with_retry
 
